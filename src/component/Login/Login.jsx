eaa1dc814e36c8d7f54f7c0fc543aeb0f3d69086
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-05-01 11:21:53 -0700

    Add authContext to login comp

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
 import './Login.scss'
 import axios from 'axios'
 import Register from '../Register /Register'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from '../Utilities/AuthContext';
 
 export default function Login() {
 
@@ -9,42 +11,85 @@ export default function Login() {
 
     const [ loginData, setLoginData ] = useState({ email: '', password: ''})
 
+    const { setAuth } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+    
+    const userRef = useRef()
+    const errRef = useRef()
+
+    // const [ user, setUser ] = useState('')
+    // const [ pwd, setPwd ] = useState('')
+    const [ errMsg, setErrMsg ] = useState('')
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [loginData])
+
     const handleLoginFormChange = (e, property) => {
         setLoginData(state => ({ ...state, [property]: e.target.value }))
     }
 
     const handleLoginSubmit = async e => {
         e.preventDefault()
-    
+        
+        try {
         // STEP 1: GET JWT FROM SERVER
         const loginRes = await axios.post(`${REACT_APP_API_BASE_PATH_USER}/login`, loginData)
         console.log(loginRes)
-    
+        
         // STEP 2: STORING JWT IN BROWSER STORAGE - localStorage, sessionStorage, cookies
         localStorage.setItem('authToken', loginRes.data.token)
+
+        const accessToken = loginRes.data.token
+        setAuth({email: loginData.email, password: loginData.password, accessToken})
+         // Reset the loginData state to clear the form fields
+        setLoginData({ email: '', password: '' });
+        } catch(err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
       }
 
     return (
         <div>
             <Register />
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
             <section>
                 <h2>Log In</h2>
                 <form onSubmit={handleLoginSubmit}>
                     <div>
-                        <label htmlFor='loginUsername'>Email</label>
+                        <label htmlFor='email'>Email</label>
                         <input
-                        type='text'
-                        value={loginData.username}
-                        onChange={e => handleLoginFormChange(e, 'email')}
-                        id='loginUsername' />
+                            type='email'
+                            value={loginData.email}
+                            ref={userRef}
+                            onChange={e => handleLoginFormChange(e, 'email')}
+                            id='email' 
+                            required
+                        />
                     </div>
                     <div>
-                        <label htmlFor='loginPass'>Password</label>
+                        <label htmlFor='password'>Password</label>
                         <input
-                        type='password'
-                        value={loginData.password}
-                        onChange={e => handleLoginFormChange(e, 'password')}
-                        id='loginPass' />
+                            type='password'
+                            value={loginData.password}
+                            onChange={e => handleLoginFormChange(e, 'password')}
+                            id='password'
+                            required
+                        />
                     </div>
                     <button>Log In</button>
                 </form>
--- src/component/Utilities/AuthContext.js
+++ src/component/Utilities/AuthContext.js
@@ -1,57 +1,37 @@
 // src/context/AuthContext.js
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
-const AuthContext = createContext();
+const AuthContext = createContext({});
 
-export const useAuth = () => useContext(AuthContext);
+// export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => { 
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    // const [ isLoading, setIsLoading ] = useState(true)
-
-    useEffect(() => {
-        const token = localStorage.getItem('authToken');
-        if (token) {
-            setIsAuthenticated(true)
-        }
-    }, []);
-
-    const login = (token) => {
-        localStorage.setItem('authToken', token);
-        setIsAuthenticated(true);
-    };
-
-    const logout = () => {
-        localStorage.removeItem('authToken');
-        setIsAuthenticated(false);
-    };
+    // const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [ auth, setAuth ] = useState({})
 
     // useEffect(() => {
-    //     const verifyToken = async () => {
-    //         const token = localStorage.getItem('authToken');
-    //         if (token) {
-    //             // Example API call to validate token
-    //             try {
-    //                 await axios.get(`${REACT_APP_API_BASE_PATH_USER}/verify-token`, { headers: { Authorization: `Bearer ${token}` } });
-    //                 setIsAuthenticated(true);
-    //             } catch (error) {
-    //                 console.error('Token validation failed', error);
-    //                 setIsAuthenticated(false);
-    //             }
-    //         }
-    //         setIsLoading(false);
-    //     };
-
-    //     verifyToken();
+    //     const token = localStorage.getItem('authToken');
+    //     if (token) {
+    //         setIsAuthenticated(true)
+    //     }
     // }, []);
 
-    // if (isLoading) {
-    //     return <div>Loading...</div>; // Or some other loading indicator
-    // }
+    // const login = (token) => {
+    //     localStorage.setItem('authToken', token);
+    //     setIsAuthenticated(true);
+    // };
+
+    // const logout = () => {
+    //     localStorage.removeItem('authToken');
+    //     setIsAuthenticated(false);
+    // };
+
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+        <AuthContext.Provider value={{ auth, setAuth }}>
             {children}
         </AuthContext.Provider>
     );
 };
+
+export default AuthContext;
