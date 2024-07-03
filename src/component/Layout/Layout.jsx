f4d6c12849cc97d78b9e46daf02fa927fca10ecc
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-07-02 17:43:46 -0700

    Removed background in _global and adjust code in layout

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,26 +1,41 @@
-import { useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import backgroundImage from '../../assets/images/background-img.jpg'
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import './Layout.scss'; // Import your CSS file
 
 const Layout = () => {
   const location = useLocation()
 
   useEffect(() => {
+ 
+    const bodyClassList = document.body.classList
+
+    const uuidPattern = /^\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
+
     if (
       location.pathname === '/recommendations' || 
-      location.pathname === '/' || location.pathname === '/dashboard' || 
-      location.pathname.match(/^\/[^/]+$/)) {
-      
-      document.body.style.backgroundImage = 'none'
-      document.body.style.backgroundColor = '#cde7f9'
+      location.pathname === '/' || 
+      location.pathname === '/dashboard' ||
+      uuidPattern.test(location.pathname)
+    ) {
+      bodyClassList.add('background-color')
+      bodyClassList.remove('background-image')
     
     } else {
-      document.body.style.backgroundImage = `url(${backgroundImage})`
-      document.body.style.backgroundColor = 'transparent'
+      console.log('Applying background image class')
+      bodyClassList.add('background-image')
+      bodyClassList.remove('background-color')
     }
+
+    // Cleanup function to reset styles
+    return () => {
+      console.log('Removing background classes')
+      bodyClassList.remove('background-color')
+      bodyClassList.remove('background-image')
+    };
+
   }, [location])
 
   return null
-}
+};
 
 export default Layout
